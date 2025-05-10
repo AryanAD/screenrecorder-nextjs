60f6081e949f356c29ce6560b266b45a7ab674ed
AryanAD <aryanhohai@gmail.com>
2025-05-10 12:46:39 +0545

    Homepage

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Page = () => {
+  return (
+    <main className="wrapper page">
+      <Header title="All Videos" subHeader="Public Library" />
+      <h1 className="font-karla text-2xl">Welcome to ScreenCapt</h1>
+    </main>
+  );
+};
+
+export default Page;
